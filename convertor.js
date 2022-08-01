2b9fbd53d44b6f0f7cf8e07a1dd28fd356c64027
1xtend <kokitoki0504@gmail.com>
2022-08-01 16:23:23 +0200

    third commit

--- convertor.js
+++ convertor.js
@@ -86,4 +86,31 @@ function updateThumbnail(dropZoneElement, file) {
   cloud.style.display = 'none';
 }
 
-// style="display: none;"
+const form = document.querySelector('#form');
+const fromInput = document.querySelector('#fromInput');
+const toInput = document.querySelector('#toInput');
+
+const input = document.querySelector('.drop-zone__input');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const file = input.files[0];
+  const format = file.type.slice(6);
+  console.log(format);
+
+  const formData = new FormData(e.target);
+  axios(
+    `https://v2.convertapi.com/convert/${format}/to/${toInput.value}?Token=DG7evB9j`,
+    {
+      method: 'POST',
+      data: formData,
+    }
+  ).then((data) => {
+    imgCode = data.data.Files[0].FileData;
+    const img = document.querySelector('#img');
+    const link = document.querySelector('#link');
+    img.src = `data:image/${toInput.value};base64, + imgCode`;
+    link.style.display = 'block';
+    link.href = `data:image/${toInput.value};base64, + imgCode`;
+  });
+});
